25d0d3b4e90111afbea2842b98fc92ce8a0db8a3
Akili ET <lennykamande@bitbucket.org>
2018-05-27 00:32:45 +0300

    Arrival Push Notification

--- app/src/main/java/com/dodrop/riderapp/Service/MyFirebaseMessaging.java
+++ app/src/main/java/com/dodrop/riderapp/Service/MyFirebaseMessaging.java
@@ -1,10 +1,17 @@
 package com.dodrop.riderapp.Service;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.Looper;
+import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
 
+
+import com.dodrop.riderapp.R;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -17,13 +24,36 @@ public class MyFirebaseMessaging extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(final RemoteMessage remoteMessage) {
-        Handler handler = new Handler(Looper.getMainLooper());
-        handler.post(new Runnable() {
-            @Override
-            public void run() {
-                Toast.makeText(MyFirebaseMessaging.this,""+remoteMessage.getNotification().getBody(),Toast.LENGTH_SHORT).show();
-            }
-        });
+        if(remoteMessage.getNotification().getTitle().equals("Cancellation")) {
+            Handler handler = new Handler(Looper.getMainLooper());
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(MyFirebaseMessaging.this, "" + remoteMessage.getNotification().getBody(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        else if(remoteMessage.getNotification().getTitle().equals("Motorbike Arrival")) {
+            showArrivedNotification(remoteMessage.getNotification().getBody());
+        }
+    }
+
+    private void showArrivedNotification(String body) {
+        PendingIntent contentIntent = PendingIntent.getActivity(getBaseContext(),0 ,
+                new Intent(), PendingIntent.FLAG_ONE_SHOT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getBaseContext());
+
+        builder.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_LIGHTS|Notification.DEFAULT_SOUND)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle("Arrived")
+                .setContentText(body)
+                .setContentIntent(contentIntent);
+        NotificationManager manager = (NotificationManager)getBaseContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(1, builder.build());
+
     }
 }
 
